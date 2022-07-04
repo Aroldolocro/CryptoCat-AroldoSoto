a00c7da8849a5b0e2fc9ab742a3409b484058506
Aroldolocro <lolosoto321@gmail.comm>
2022-07-04 20:00:13 -0300

    Desafío 4

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -10,29 +10,36 @@ const incrementar = () => {
     setCount(count + 1);
 };
 const decrementar = () => {
-    setCount((valAnt) => (valAnt > 0 ? count - 1 : count))
+    setCount((valAnt) => (valAnt > 1 ? count - 1 : count))
 };
 
   return (
     <div className='itemcount-c'> 
-      <div className='img-box'></div>
-      <div className='box-layout'>
-      <p className='box b1'>USDT</p>
-      <p className='box b2'>{count}</p>
-      <button onClick={decrementar} className='box b3'>-</button>
-      <button onClick={incrementar} className='box b4'>+</button>
-      <button className='box b5'
-        onClick={() => {
-            if (count <= stock) {
-                onAdd(count);
-            }
-            else {
-                alert("No hay stock")
-            }
-        }} >
-            COMPRAR
-      </button>
+    
+      <div className='C-1'></div>
+
+      <div className='C-2'>
+
+      <div className='C-2-B1'><p className='txt-1'>USDT</p></div>
+      <div className='C-2-B2'>
+        <button className='C-2-B2B1' onClick={decrementar}><p className='txt-2'>-</p></button>
+        <div className='C-2-B2B2'>{count}</div>
+        <button className='C-2-B2B3' onClick={incrementar}><p className='txt-2'>+</p></button>
       </div>
+      <button className='C-2-B3'
+      onClick={() => {
+          if (count <= stock) {
+              onAdd(count);
+          }
+          else {
+              alert("No hay stock")
+          }
+      }} >
+          <p className='txt-3'>COMPRAR</p>
+  </button>
+
+      </div>
+
     </div>
   )
 }
